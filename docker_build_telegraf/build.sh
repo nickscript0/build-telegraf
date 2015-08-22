7464dbcc9f881e972218dbcf7bbaf4337deeb52d
nickscript0 <nocl123@gmail.com>
2015-08-22 13:56:22 -0600

    develop out of forked repo

--- docker_build_telegraf/build.sh
+++ docker_build_telegraf/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 cd /go
 echo "Running go get..."
-go get -u github.com/influxdb/telegraf/...
+#go get -u github.com/influxdb/telegraf/...
+go get -u github.com/nickscript0/telegraf/...
 cd /go/src/github.com/influxdb/telegraf
 echo "Running make..."
 make
